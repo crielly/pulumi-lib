e3256901abc97e7cea195ebbe01fa82f63a4116b
Connor Rielly <crielly@outlook.com>
2020-12-14 22:02:17 -0500

    implement a GetIntWithDefault method

---
+++ getters/getInt.go
@@ -0,0 +1,20 @@
+package getters
+
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
+)
+
+// GetIntWithDefault is used to get a value if present, but return a default if not
+func GetIntWithDefault(cfg config.Config, key string, defValue int) (value pulumi.Int) {
+
+	cfgValue := cfg.GetInt(key)
+
+	if cfgValue != 0 {
+		value = pulumi.Int(cfgValue)
+	} else {
+		value = pulumi.Int(defValue)
+	}
+
+	return
+}
